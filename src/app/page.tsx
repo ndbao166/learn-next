8a436adf02cd8eccf0465f95dab5aa5cd0df814f
ndbao166 <nguyenducbaocv@gmail.com>
2026-05-01 15:22:14 +0700

    đa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { getPost } from "@/lib";
-import { cookies } from "next/headers";
 import SlowComponent from "@/components/SlowComponent";
 import { Suspense } from "react";
 
 export default async function Home() {
-  // await cookies(); test ở production
   const posts = await getPost();
   return <div>
     <h1>Posts</h1>
--- src/components/SlowComponent.tsx
+++ src/components/SlowComponent.tsx
@@ -1,18 +1,17 @@
 import { getPost } from "@/lib";
-import VerrySlowComponent from "@/components/VerrySlowComponent";
+import { connection } from "next/server";
 
-import React, { Suspense } from 'react'
 
 const SlowComponent = async () => {
+
+  await connection()
+  
   const posts = await getPost();
   return (
     <div>
         <h1>Posts 2 (Slow Component)</h1>
         <p>{posts.title}</p>
         <p>{posts.content}</p>
-        <Suspense fallback={<div>Loading...</div>}>
-            <VerrySlowComponent />
-        </Suspense>
     </div>
   )
 }
